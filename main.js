29a14fbdc04051805ec642180a29ce4ba0d71810
Silent <hamidreza.neo@gmail.com>
2021-08-22 15:34:17 +0430

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
-const TeleBot = require('telebot');
+const TeleBot = require('telebot')
 
-const dotenv = require('dotenv').config();
-const ytdl = require('ytdl-core');
-const { exec } = require("child_process");
-const fs = require('fs');
-// replace the value in .env file with the Telegram token you receive from @BotFather
-const token = process.env.API_KEY;
-const bot = new TeleBot(token);
+require('dotenv').config()
+const token = process.env.API_KEY
+const bot = new TeleBot(token)
 
-bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
-
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'))
 
 bot.on('text', async (msg) => {
     chatID = msg.chat.id;
-    return bot.sendMessage(chatID, "Ahhhh bokon tooom");
-    
-});
+    return bot.sendMessage(chatID, "Ahhhh bokon tooom")
+})
 
-bot.start();
+bot.start()
