a34a7c02d1e2474e9fb205a6042bb7e035353d3f
Silent <hamidreza.neo@gmail.com>
2021-08-22 15:50:09 +0430

    .

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const TeleBot = require('telebot')
-
+const searchYT = require('yt-search')
 require('dotenv').config()
+
 const token = process.env.API_KEY
 const bot = new TeleBot(token)
+
+async function findVideo(query) {
+    const result = await searchYT(query)
+    return (result.videos.length > 1) ? result.videos[0] : null
+}
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'))
 
 bot.on('text', async (msg) => {
-    chatID = msg.chat.id
-    return bot.sendMessage(chatID, "Ahhhh bokon tooom")
+    const chatID = msg.chat.id
+    const video = await findVideo(msg.text)
+
+    bot.sendMessage(chatID, `Downloading ${video.title}...`)
+        .then(_ => {
+            
+        })
 })
 
 bot.start()
