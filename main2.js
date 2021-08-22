5ea01a70aaa77e45d88dfcf2328ac0530e596127
zNiGhTFuRyZNTT <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2021-08-22 21:00:30 +1000

    !First Commit

--- main2.js
+++ main2.js
@@ -1,11 +1,20 @@
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const TeleBot = require('telebot');
+
 const dotenv = require('dotenv').config();
 const ytdl = require('ytdl-core');
 const { exec } = require("child_process");
 const fs = require('fs');
 // replace the value in .env file with the Telegram token you receive from @BotFather
 const token = process.env.API_KEY;
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TeleBot(token);
+
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
 
 
+bot.on('text', async (msg) => {
+    chatID = msg.chat.id;
+    return bot.sendMessage(chatID, "Ahhhh bokon tooom");
+    
+});
 
+bot.start();
