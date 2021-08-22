055b488a7f230d863f0cf23e9080aa45a8482ae6
Silent <hamidreza.neo@gmail.com>
2021-08-22 16:15:59 +0430

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const TeleBot = require('telebot')
 const searchYT = require('yt-search')
+const YoutubeDlWrap = require("youtube-dl-wrap")
+const youtubeDlWrap = new YoutubeDlWrap("./youtube-dl")
 require('dotenv').config()
-const fs = require('fs');
-const ytdl = require('ytdl-core');
+
 const token = process.env.API_KEY
 const bot = new TeleBot(token)
 
@@ -19,8 +20,11 @@ bot.on('text', async (msg) => {
 
     bot.sendMessage(chatID, `Downloading ${video.title}...`)
         .then(async _ => {
-            await ytdl(video.url).pipe(fs.createWriteStream(`storage/${video.title}.mp3`));
-            bot.sendAudio(chatID, `storage/${video.title}.mp3`)
+            const path = `storage/${video.title}.mp3`
+            youtubeDlWrap.exec(['--extract-audio', '--audio-format mp3', video.url, "-o", path])
+                .on('close', () => {
+                    bot.sendAudio(chatID, `storage/${video.title}.mp3`)
+                })
         })
 })
 
