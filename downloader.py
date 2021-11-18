5e3ea1bae7905bbb1dec4dbe3f7238fe2302404d
NF <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2021-11-19 06:46:05 +1100

    [🍑] update quality And Legend Update

--- downloader.py
+++ downloader.py
@@ -7,9 +7,9 @@ msgID = sys.argv[3]
 
 def main(url, chatID, msgID):
     yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
+    video = [i for i in yt.streams.filter(only_audio=True).all() if i.mime_type == "audio/mp4"][-1]
+    print(video)
     downloaded_file = video.download()
-    base, ext = os.path.splitext(downloaded_file)
     new_file = f"storage/{chatID}-{msgID}.mp3"
     os.rename(downloaded_file, new_file)
 
