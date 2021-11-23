249536e1b7f7b4f4dd7085fb36efac265c6e8258
MainSilent <hamidreza.neo@gmail.com>
2021-11-23 18:51:06 +0330

    fix admin queries

--- admin.js
+++ admin.js
@@ -1,13 +1,13 @@
 const { getUser } = require('./database')
 
 function sendToUser(bot, msg, chat_id, message) {
-    bot.sendMessage(chat_id, `[❗️] Sent by Admin:\n\n    ${message}`)
+    bot.sendMessage(chat_id, `[❗️] Sent by Admin:\n\n${message}`)
         .catch(err => msg.reply.text(`[❗️] Error Sending message: ${err.message}`)) 
 }
 
 function searchUser(msg, user_id) {
     getUser(user_id)
-        .then(res => msg.reply.text(`[User]:\n   username: ${res.username},\n   firstname= ${res.firstname},\n  lastname= ${res.lastname}\n   user_id= ${res.user_id},\n   chat_id= ${res.chat_id}`))
+        .then(res => msg.reply.text(`[User]:\n   username: ${res.username}\n   firstname= ${res.firstname}\n   lastname= ${res.lastname}\n   user_id= ${res.user_id}\n   chat_id= ${res.chat_id}`))
         .catch(err => msg.reply.text(`[!] Failed : ${err}`))
 }
 
