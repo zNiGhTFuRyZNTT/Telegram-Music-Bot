14bc78f3230fe69238174eaa96e0163d906911b1
MainSilent <70273681+MainSilent@users.noreply.github.com>
2021-11-29 10:53:31 +0330

    Update forwardAll.js

--- forwardAll.js
+++ forwardAll.js
@@ -12,7 +12,7 @@ getAllUsers()
             bot.forwardMessage(user.chat_id, -1001404127129, 16)
             .then(() => console.log(`Sending to ${user.firstname} `+"\033[32m"+"Success"+"\033[0m"))
             .catch(e => {
-                fs.appendFile('error.log', `\n${user.user_id} - ${JSON.stringify(e)}\n`, (err) => err && console.error(err))
+                appendFile('error.log', `\n${user.user_id} - ${JSON.stringify(e)}\n`, (err) => err && console.error(err))
 
                 console.log(`Sending to ${user.firstname} `+"\033[31m"+"Failed"+"\033[0m")
             })
