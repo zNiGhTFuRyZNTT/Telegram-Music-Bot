ef098893b50f257a5174039b51b0b5b3d8506e08
MainSilent <70273681+MainSilent@users.noreply.github.com>
2021-11-29 10:59:23 +0330

    Update forwardAll.js

--- forwardAll.js
+++ forwardAll.js
@@ -3,19 +3,22 @@ const { appendFile } = require('fs')
 const { getAllUsers } = require('./database')
 require('dotenv').config()
 
+const interval = 100
 const token = process.env.API_KEY
 const bot = new TeleBot(token)
 
 getAllUsers()
     .then(users => {
-        users.forEach(user => {
-            bot.forwardMessage(user.chat_id, -1001404127129, 16)
-            .then(() => console.log(`Sending to ${user.firstname} `+"\033[32m"+"Success"+"\033[0m"))
-            .catch(e => {
-                appendFile('error.log', `\n${user.user_id} - ${JSON.stringify(e)}\n`, (err) => err && console.error(err))
+        users.forEach((user, i) => {
+            setTimeout(() => {
+                bot.forwardMessage(user.chat_id, -1001404127129, 16)
+                    .then(() => console.log(`Sending to ${user.firstname} `+"\033[32m"+"Success"+"\033[0m"))
+                    .catch(e => {
+                        appendFile('error.log', `\n${user.user_id} - ${JSON.stringify(e)}\n`, (err) => err && console.error(err))
 
-                console.log(`Sending to ${user.firstname} `+"\033[31m"+"Failed"+"\033[0m")
-            })
+                        console.log(`Sending to ${user.firstname} `+"\033[31m"+"Failed"+"\033[0m")
+                    })
+            }, i * interval)
         })
     })
     .catch(err => console.error(err))
